a536f2413b39552e6bea703d4630364e61b1d5e1
mudassar-web <mudassar.ansari@gmail.com>
2023-02-05 20:16:21 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// Add Express
+const express = require("express");
+const PORT = process.env.PORT || 4000
+
+// Initialize Express
+const app = express();
+
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("MD-Express-Server Running on Vercel");
+});
+
+// health checkup call
+app.get('/health', (_req, res) => {
+  const data = {
+    uptime: process.uptime(),
+    message: 'Ok',
+    date: new Date()
+  }
+  res.status(200).send(data)
+})
+
+// Initialize server
+app.listen(PORT, () => {
+  console.log(`Sever Running on port ${PORT}.\nhttp://localhost:${PORT}`);
+});
+
+module.exports = app
